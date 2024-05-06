d7ee84ba8d5592c64476ace9821aa5182cbbefbd
Administrator <188405526658@163.com>
2024-05-06 13:40:03 +0800

    我的第6次推送

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ def print_hi4(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
+def print_h6(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
