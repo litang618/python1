367358265aca490a82ab500b9fefcc823c3c4ac2
Administrator <188405526658@163.com>
2024-05-06 13:35:26 +0800

    3

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def print_hi(name):
 def print_hi1(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def print_hi2(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
