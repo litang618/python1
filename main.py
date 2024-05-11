8ba1a81cd02916f8d73154616c02063aaa4fefa0
Administrator <188405526658@163.com>
2024-05-11 16:28:58 +0800

    11

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-alien_0 = {'color': 'green', 'points': 5}
-alien_1 = {'color': 'yellow', 'points': 10}
-alien_2 = {'color': 'red', 'points': 15}
-aliens = [alien_0, alien_1, alien_2]
-for alien in aliens:
+# 创建一个用于存储外星人的空列表
+aliens = []
+# 创建 30 个绿色的外星人
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+# 显示前 5 个外星人
+for alien in aliens[:5]:
     print(alien)
+print("...")
